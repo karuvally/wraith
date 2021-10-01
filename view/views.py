6ffabad5040b4bf5338a472ac81b509b5e4ee1b7
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-10-01 20:51:05 +0530

    added overview view for view app

--- view/views.py
+++ view/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class Overview(View):
+    template_name = "overview.html"
+    
+    def get(self, request):
+        return render(request, template_name, {})    
