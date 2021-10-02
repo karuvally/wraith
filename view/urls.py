992b8d99d8c13fc31adc8659e9ae17f1fd1bc325
Aswin Karuvally <aswinbabuk@gmail.com>
2021-10-02 21:29:48 +0530

    removed view app

--- view/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import Overview
-
-app_name = "view"
-urlpatterns = [
-    path("", Overview.as_view(), name="overview"),
-]
--- view/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views import View
-
-# Create your views here.
-class Overview(View):
-    template_name = "overview.html"
-    
-    def get(self, request):
-        return render(request, self.template_name, {})    
