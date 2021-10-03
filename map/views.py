840727a6c5a5c865ec20766c1e19b0b8e2b72cb8
Aswin Karuvally <aswinbabuk@gmail.com>
2021-10-03 20:32:15 +0530

    added test page for map app

--- map/views.py
+++ map/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
+class HomeView(View):
+    def get(self, request):
+        return HttpResponse("This works!")
