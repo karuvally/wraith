b8097687adab007d943fd8ec08c8e0f244455594
Aswin Karuvally <aswinbabuk@gmail.com>
2021-10-03 20:19:18 +0530

    removed urls for deleted app view

--- wraith/urls.py
+++ wraith/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('login.urls')),
-    path('overview', include('view.urls')),
 ]
