f06679e510fa1655f5efd674c85014ba7edd15d8
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-10-04 16:10:32 +0530

    added upload PDF page

--- pdf_to_img/views.py
+++ pdf_to_img/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class UploadPDF(View):
+    def get(self, request):
+        return render(request, "upload_pdf.html", {})
