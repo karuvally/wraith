813b4842c2e89cc07f6ed4c0b8d74c5588e64ff1
Aswin Karuvally <aswinbabuk@gmail.com>
2021-10-09 16:54:19 +0530

    added file upload functionality

--- pdf_to_img/views.py
+++ pdf_to_img/views.py
@@ -1,5 +1,6 @@
 import string
 import random
+import os
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
@@ -12,6 +13,10 @@ class UploadPDF(View):
     def post(self, request):
         if not request.FILES:
             return HttpResponse("Not a valid file...")
-        tmpfile = f"{''.join(random.choices(string.ascii_letters, k=10))}.pdf"
-        request.session["pdf_upload"] = request.FILES["pdf_file"]
+        # Save uploaded file with temporary name
+        fname = f"{''.join(random.choices(string.ascii_letters, k=10))}.pdf"
+        with open(os.path.join("pdf_to_img", fname), "wb") as tmp_file:
+            for chunk in request.FILES["pdf_file"].chunks():
+                tmp_file.write(chunk)
+        request.session["pdf_file"] = fname
         return render(request, "configuration.html", {})
