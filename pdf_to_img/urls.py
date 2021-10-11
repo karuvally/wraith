4095c5645c38fe5892466c846dadaff9ea1c9aac
Aswin Karuvally <aswinbabuk@gmail.com>
2021-10-11 16:00:11 +0530

    added URL for convert.html

--- pdf_to_img/urls.py
+++ pdf_to_img/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import UploadPDF
+from .views import UploadPDF, ConvertPDF
 
 app_name = "pdf_to_img"
 urlpatterns = [
-    path('upload/', UploadPDF.as_view(), name="upload_pdf"),
+    path("upload/", UploadPDF.as_view(), name="upload_pdf"),
+    path("convert/", ConvertPDF.as_view(), name="convert_pdf"),
 ]
